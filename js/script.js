f369077239205069b2c464d39368d0ca27d4d934
RevyRobinson <revychu@hotmail.com>
2016-11-23 13:42:28 -0300

    modificacion a html flags

--- js/script.js
+++ js/script.js
@@ -1,7 +1,15 @@
 //seteando los paises
-var chile = {flag:'image/flag-chile.png',name:'Chile',carrier: '+569',max:'8'};
-var peru = {flag:'image/flag-peru.png',name:'Perú',carrier:'+51',max:'9'};
-var mexico = {flag:'image/flag-mexico.png',name:'México',carrier:'+521',max:'10'};
-var usa = {flag:'image/flag-usa.png',name:'Estados Unidos',carrier:'+1',max:'10'};
+var chile = {flag:'<img src="image/flag-chile.png">',name:'Chile',carrier: '+569',max:'8'};
+var peru = {flag:'<img src="image/flag-peru.png">',name:'Perú',carrier:'+51',max:'9'};
+var mexico = {flag:'<img src="image/flag-mexico.png">',name:'México',carrier:'+521',max:'10'};
+var usa = {flag:'<img src="image/flag-usa.png">',name:'Estados Unidos',carrier:'+1',max:'10'};
 
-$("#demo").intlTelInput();
+//$("#demo").intlTelInput();
+
+function countrySelector(x){
+//	document.getElementById('country-option').innerHTML =  x[0];
+	// Store
+	localStorage.setItem("flag", x[0]); //?????????
+	// Retrieve
+	document.getElementById('country-option').innerHTML = localStorage.getItem("flag");
+}
