87cd1efd60fd7bd024647830f2136be4a0cc78bc
Alice Barker <alicebarker@Carloss-MBP.iptv\.microsoft\.com>
2016-11-23 01:37:44 -0300

    plugin banderas, intento de html con banderas, objetos con datos de los paises

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//seteando los paises
+var chile = {flag:'image/flag-chile.png',name:'Chile',carrier: '+569',max:'8'};
+var peru = {flag:'image/flag-peru.png',name:'Perú',carrier:'+51',max:'9'};
+var mexico = {flag:'image/flag-mexico.png',name:'México',carrier:'+521',max:'10'};
+var usa = {flag:'image/flag-usa.png',name:'Estados Unidos',carrier:'+1',max:'10'};
+
+$("#demo").intlTelInput();
