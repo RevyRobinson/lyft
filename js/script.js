dfe45e42e18d019f7daa7a68e3e53f3e1ee26905
RevyRobinson <revychu@hotmail.com>
2016-11-29 09:19:31 -0300

    cambios que quiero guardar

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ var peru = {flag:'<img src="image/flag-peru.png">',name:'Perú',carrier:'+51',ma
 var mexico = {flag:'<img src="image/flag-mexico.png">',name:'México',carrier:'+521',max:'10'};
 var usa = {flag:'<img src="image/flag-usa.png">',name:'Estados Unidos',carrier:'+1',max:'10'};
 
-//$("#demo").intlTelInput();
+$("#demo").intlTelInput();
 
-function countrySelector(x){
+/*nction countrySelector(pais){
 //	document.getElementById('country-option').innerHTML =  x[0];
 	// Store
-	localStorage.setItem("flag", x[0]); //?????????
+	localStorage.setItem("flag", pais[0]); //?????????
 	// Retrieve
 	document.getElementById('country-option').innerHTML = localStorage.getItem("flag");
-}
+}*/
